d9cca0bcd32dafce272edce610abcc13fc583fd1
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2021-08-15 23:15:31 +0500

    dropdown value from internet has been fixed

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,3 +1,7 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
 class Products {
   final int id;
   final String name;
@@ -10,15 +14,31 @@ class Products {
         name:['name'].toString(),
     );
   }
-
   static List<Products> fetchData({List jsonList}) {
     List<Products> list = [];
 
     for (int i = 0; i < jsonList.length; i++) {
       list.add(Products.fromJson(json: jsonList[i]));
     }
-
     return list;
   }
 
+}
+
+Future<List<Products>> getProducts() async {
+  try {
+    final http.Response response = await http.get(
+        Uri.parse('https://api.spector77.uz/rest/sales/products')
+    );
+    if (response.statusCode == 200) {
+      // print('the body is '+(response.body));
+      return Products.fetchData(jsonList: jsonDecode(response.body));
+    }
+    else {
+      throw Exception('Failed to load sales');
+    }
+
+  }catch(e) {
+    print(e);
+  }
 }
--- lib/pages/sale_form.dart
+++ lib/pages/sale_form.dart
@@ -1,95 +1,50 @@
-import 'dart:convert';
-import 'package:dropdownfield/dropdownfield.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/products.dart';
-import 'package:flutter_app/models/products.dart';
-import 'package:flutter_app/models/products.dart';
-import 'package:flutter_app/models/singleSale.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:searchable_dropdown/searchable_dropdown.dart';
+import 'package:flutter_app/models/userApi.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
-
-
-Future<List<Products>> getProducts() async {
-  try {
-    final http.Response response = await http.get(
-        Uri.parse('https://api.spector77.uz/rest/sales/products}')
-    );
-    if (response.statusCode == 200) {
-      // print('the body is '+(response.body));
-      return Products.fetchData(jsonList: jsonDecode(response.body));
-    }
-    else {
-      throw Exception('Failed to load sales');
-    }
-
-  }catch(e) {
-    print(e);
-  }
-}
-
-
-class SaleForm extends StatefulWidget {
+class NetworkTypeAheadPage extends StatelessWidget {
   @override
-  _SaleFormState createState() => _SaleFormState();
-}
-
-class _SaleFormState extends State<SaleForm> {
-  Future<List<Products>> products;
-
-  @protected
-  void initState() {
-    super.initState();
-    products = getProducts();
-    // user_id = _loadCounter().toString();
-    // print('user_id: '+user_id);
-  }
-
-  String country_id='';
-  List<String> country = [
-    "America",
-    "Brazil",
-    "Canada",
-    "India",
-    "Mongalia",
-    "USA",
-    "China",
-    "Russia",
-    "Germany"
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Sotilgan maxsulotlar: '+country_id),
-        ),
-        body:Column(
-          children: [
-            Text('dropdown uchun'+country_id),
-            DropDownField(
-              onValueChanged: (value) {
-                setState(() {
-                  country_id = value;
-                });
-              },
-              value: country_id,
-              required: false,
-              hintText: 'Choose a country',
-              labelText: 'Country',
-              items: country,
+  Widget build(BuildContext context) => Scaffold(
+    body: SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: TypeAheadField<User>(
+          hideSuggestionsOnKeyboardHide: false,
+          textFieldConfiguration: TextFieldConfiguration(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+              hintText: 'Search Username',
             ),
-          ],
-        )
+          ),
+          suggestionsCallback: UserApi.getUserSuggestions,
+          itemBuilder: (context, User suggestion) {
+            final user = suggestion;
 
-    );
-  }
+            return ListTile(
+              title: Text(user.name),
+            );
+          },
+          noItemsFoundBuilder: (context) => Container(
+            height: 100,
+            child: Center(
+              child: Text(
+                'No Users Found.',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+          onSuggestionSelected: (User suggestion) {
+            final user = suggestion;
+
+            ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(SnackBar(
+                content: Text('Selected user: ${user.name}'),
+              ));
+          },
+        ),
+      ),
+    ),
+  );
 }
-
-
-
-
-
